09d96ee5b50739cabee72f3dfc41d7771fb85fab
kanemj <matthewjkane@live.com>
2021-01-19 12:51:58 -0800

    added ratings section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to CoffeeShop</h1>
+      <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
